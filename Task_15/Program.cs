dfefd54a8af03e771ac1d006e354aa30830e9a96
Konstantin Senchenkov <senchenkovko@gmail.com>
2023-01-31 18:48:37 +0400

    Solve task_15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num > 5 && num <= 7)
+{
+    Console.WriteLine($"да");
+}
+if(num >= 1 && num == 5)
+{
+    Console.WriteLine($"нет");
+}
