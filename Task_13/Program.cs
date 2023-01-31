e8bce036744d201e2f68b1ae2980faae7157b791
Konstantin Senchenkov <senchenkovko@gmail.com>
2023-01-31 18:41:15 +0400

    Solve task_13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+int count = num.Length;
+if(count >= 3)
+{
+    string num1 = Convert.ToString(num[2]);
+    Console.WriteLine($"{num} -> {num1}");
+}
+if(count <= 2)
+{
+    Console.WriteLine($"третьей цифры нет");
+}
